97c726ef85acb840954d3778e0ddb3ece137c4be
0xhaz <28639279+0xhaz@users.noreply.github.com>
2024-11-28 03:17:59 +0800

    Added Script

---
+++ foundry/script/DeployBeanHeads.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.26;
+
+import {Script, console} from "forge-std/Script.sol";
+import {BeanHeads} from "src/core/BeanHeads.sol";
+
+contract DeployBeanHeads is Script {
+    function run() public returns (BeanHeads) {}
+}
