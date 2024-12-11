081681179ee19c4fdafc3171c1d69a95ebc252b2
0xhaz <28639279+0xhaz@users.noreply.github.com>
2024-12-11 12:23:45 +0800

    Added Library

---
+++ foundry/src/libraries/SVGBody.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.26;
+
+library SVGBody {
+    /// @dev Generates an SVG Wrapper for a component
+    /// @param tag the SVG tag (e.g., "g", "circle", etc)
+    /// @param attributes The attributes for the tag (e.g, 'id="group1")
+    /// @param children Nested SVG content
+    function base(string memory tag, string memory attributes, string memory children)
+        internal
+        pure
+        returns (string memory)
+    {
+        string memory openingTag = bytes(attributes).length > 0
+            ? string(abi.encodePacked("<", tag, " ", attributes, ">"))
+            : string(abi.encodePacked("<", tag, ">"));
+
+        return string(abi.encodePacked(openingTag, children, "</", tag, ">"));
+    }
+}
