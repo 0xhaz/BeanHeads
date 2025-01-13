9102143e729434d910ed3ea2245b82676b7e2f5e
0xhaz <28639279+0xhaz@users.noreply.github.com>
2025-01-13 09:33:29 +0800

    Added NatSpec for Genesis

--- foundry/src/types/Genesis.sol
+++ foundry/src/types/Genesis.sol
@@ -65,6 +65,79 @@ library Genesis {
         OtherParams otherParams;
     }
 
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTION
+    //////////////////////////////////////////////////////////////*/
+    /**
+     * @notice Builds an SVG string for the avatar based on the provided parameters.
+     * @dev Uses components and styles to construct the final SVG.
+     * @param params A `SVGParams` struct containing details for avatar customization.
+     * @return svg The complete SVG string representing the avatar.
+     */
+    function buildAvatar(SVGParams memory params) internal pure returns (string memory) {
+        (
+            string memory hairBack,
+            string memory hairFront,
+            string memory hairSVG,
+            string memory accessorySVG,
+            string memory bodySVG,
+            string memory clothesSVG,
+            string memory clothingGraphicSVG,
+            string memory eyebrowShapeSVG,
+            string memory eyeSVG,
+            string memory facialHairSVG,
+            string memory hatSVG,
+            string memory mouthNameSVG,
+            string memory shapeSVG,
+            string memory faceMaskSVG
+        ) = getSVG(params);
+
+        string memory svg = string(
+            abi.encodePacked(
+                '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">',
+                shapeSVG,
+                hairBack,
+                bodySVG,
+                hairFront,
+                clothesSVG,
+                hairSVG,
+                clothingGraphicSVG,
+                eyeSVG,
+                facialHairSVG,
+                hatSVG,
+                mouthNameSVG,
+                eyebrowShapeSVG,
+                accessorySVG,
+                faceMaskSVG,
+                isLashesOn(params.facialFeaturesParams.eyeShape, params.otherParams.lashes)
+            )
+        );
+
+        return svg;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            PRIVATE FUNCTION
+    //////////////////////////////////////////////////////////////*/
+    /**
+     * @notice Retrieves the SVG components for the avatar.
+     * @dev Combines hair, body, clothing, and other components into separate SVG strings.
+     * @param params A `SVGParams` struct containing details for avatar customization.
+     * @return hairBack The SVG string for the back of the hair.
+     * @return hairFront The SVG string for the front of the hair.
+     * @return hairSVG The SVG string for hair details.
+     * @return accessorySVG The SVG string for accessories.
+     * @return bodySVG The SVG string for the body.
+     * @return clothesSVG The SVG string for the clothing.
+     * @return clothingGraphicSVG The SVG string for clothing graphics.
+     * @return eyebrowShapeSVG The SVG string for eyebrows.
+     * @return eyeSVG The SVG string for eyes.
+     * @return facialHairSVG The SVG string for facial hair.
+     * @return hatSVG The SVG string for hats.
+     * @return mouthNameSVG The SVG string for the mouth.
+     * @return shapeSVG The SVG string for shapes.
+     * @return faceMaskSVG The SVG string for face masks.
+     */
     function getSVG(SVGParams memory params)
         private
         pure
@@ -121,48 +194,13 @@ library Genesis {
         (faceMaskSVG,) = isFaceMaskOn(params.otherParams.faceMask, params.otherParams.faceMaskColor);
     }
 
-    function buildAvatar(SVGParams memory params) internal pure returns (string memory) {
-        (
-            string memory hairBack,
-            string memory hairFront,
-            string memory hairSVG,
-            string memory accessorySVG,
-            string memory bodySVG,
-            string memory clothesSVG,
-            string memory clothingGraphicSVG,
-            string memory eyebrowShapeSVG,
-            string memory eyeSVG,
-            string memory facialHairSVG,
-            string memory hatSVG,
-            string memory mouthNameSVG,
-            string memory shapeSVG,
-            string memory faceMaskSVG
-        ) = getSVG(params);
-
-        string memory svg = string(
-            abi.encodePacked(
-                '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">',
-                shapeSVG,
-                hairBack,
-                bodySVG,
-                hairFront,
-                clothesSVG,
-                hairSVG,
-                clothingGraphicSVG,
-                eyeSVG,
-                facialHairSVG,
-                hatSVG,
-                mouthNameSVG,
-                eyebrowShapeSVG,
-                accessorySVG,
-                faceMaskSVG,
-                isLashesOn(params.facialFeaturesParams.eyeShape, params.otherParams.lashes)
-            )
-        );
-
-        return svg;
-    }
-
+    /**
+     * @notice Retrieves the SVG string and name for the face mask if enabled.
+     * @param faceMask A boolean indicating if the face mask is enabled.
+     * @param color The color ID for the face mask.
+     * @return maskSVG The SVG string for the face mask.
+     * @return name The name of the face mask color.
+     */
     function isFaceMaskOn(bool faceMask, uint8 color)
         private
         pure
@@ -175,6 +213,13 @@ library Genesis {
         return ("", "");
     }
 
+    /**
+     * @notice Retrieves the SVG string and name for shapes if enabled.
+     * @param shapes A boolean indicating if shapes are enabled.
+     * @param color The color ID for the shape.
+     * @return shape The SVG string for the shape.
+     * @return name The name of the shape color.
+     */
     function isShapesOn(bool shapes, uint8 color) private pure returns (string memory shape, string memory name) {
         if (shapes) {
             (shape, name) = OptItems.shapeSVG(color);
@@ -183,6 +228,12 @@ library Genesis {
         return ("", "");
     }
 
+    /**
+     * @notice Retrieves the SVG string for eyelashes if enabled.
+     * @param eyeShape The ID of the eye shape.
+     * @param lashes A boolean indicating if lashes are enabled.
+     * @return svg The SVG string for eyelashes.
+     */
     function isLashesOn(uint8 eyeShape, bool lashes) private pure returns (string memory) {
         if (lashes) {
             return OptItems.lashesSVG(eyeShape);
@@ -191,6 +242,13 @@ library Genesis {
         return "";
     }
 
+    /**
+     * @notice Retrieves the SVG strings for the back and front components of the hair.
+     * @param id The ID of the hair style.
+     * @param color The color ID for the hair.
+     * @return back The SVG string for the back of the hair.
+     * @return front The SVG string for the front of the hair.
+     */
     function getHairComponentsById(uint8 id, uint8 color)
         private
         pure
@@ -207,6 +265,11 @@ library Genesis {
         }
     }
 
+    /**
+     * @notice Builds the JSON array of attributes for the NFT metadata.
+     * @param params A `SVGParams` struct containing details for avatar customization.
+     * @return attributes A JSON string representing the attributes array.
+     */
     function buildAttributes(Genesis.SVGParams memory params) internal pure returns (string memory) {
         // Initialize JSON string
         string memory attributes = "[";
@@ -267,6 +330,11 @@ library Genesis {
         return attributes;
     }
 
+    /**
+     * @notice Builds the attributes for hair style and color.
+     * @param params A `HairParams` struct containing hair style and color details.
+     * @return hairAttributes An array of two JSON strings representing hair attributes.
+     */
     function buildHairAttributes(HairParams memory params) private pure returns (string[2] memory hairAttributes) {
         string memory hairStyleName = HairDetail.getHairTypeName(params.hairStyle);
         string memory hairColorName = HairDetail.getHairColorName(params.hairColor);
@@ -278,6 +346,11 @@ library Genesis {
         return hairAttributes;
     }
 
+    /**
+     * @notice Builds the attributes for accessories, hat style, and hat color.
+     * @param params An `AccessoryParams` struct containing accessory details.
+     * @return accessoryAttributes An array of three JSON strings representing accessory attributes.
+     */
     function buildAccessoryAttributes(AccessoryParams memory params)
         private
         pure
@@ -296,6 +369,11 @@ library Genesis {
         return accessoryAttributes;
     }
 
+    /**
+     * @notice Builds the attributes for body type and skin color.
+     * @param params A `BodyParams` struct containing body type and skin color details.
+     * @return bodyAttributes An array of two JSON strings representing body attributes.
+     */
     function buildBodyAttributes(BodyParams memory params) private pure returns (string[2] memory bodyAttributes) {
         string memory bodyTypeName = BodyDetail.getBodyTypeName(params.bodyType);
         string memory skinColorName = BodyDetail.getBodyColorName(params.skinColor);
@@ -307,6 +385,11 @@ library Genesis {
         return bodyAttributes;
     }
 
+    /**
+     * @notice Builds the attributes for clothing, clothing color, and graphic.
+     * @param params A `ClothingParams` struct containing clothing details.
+     * @return clothingAttributes An array of three JSON strings representing clothing attributes.
+     */
     function buildClothingAttributes(ClothingParams memory params)
         private
         pure
@@ -327,6 +410,11 @@ library Genesis {
         return clothingAttributes;
     }
 
+    /**
+     * @notice Builds the attributes for facial features, including eyebrows, eyes, facial hair, and lips.
+     * @param params A `FacialFeaturesParams` struct containing facial feature details.
+     * @return facialFeaturesAttributes An array of five JSON strings representing facial features attributes.
+     */
     function buildFacialFeaturesAttributes(FacialFeaturesParams memory params)
         private
         pure
@@ -355,6 +443,11 @@ library Genesis {
         return facialFeaturesAttributes;
     }
 
+    /**
+     * @notice Builds the attributes for optional items such as face masks, shapes, and lashes.
+     * @param params An `OtherParams` struct containing optional item details.
+     * @return otherAttributes An array of five JSON strings representing optional item attributes.
+     */
     function buildOtherAttributes(OtherParams memory params) private pure returns (string[5] memory otherAttributes) {
         string memory shapeColor = OptItems.getShapeColor(params.shapeColor);
         string memory faceMaskColor = OptItems.getFaceMaskColor(params.faceMaskColor);
@@ -376,6 +469,11 @@ library Genesis {
         return otherAttributes;
     }
 
+    /**
+     * @notice Generates a Base64-encoded SVG string for the avatar.
+     * @param params A `SVGParams` struct containing details for avatar customization.
+     * @return svg A Base64-encoded string of the avatar's SVG.
+     */
     function generateBase64SVG(Genesis.SVGParams memory params) internal pure returns (string memory) {
         string memory svg = string(
             abi.encodePacked(
@@ -392,6 +490,13 @@ library Genesis {
         return string(abi.encodePacked("data:image/svg+xml;base64,", Base64.encode(bytes(svg))));
     }
 
+    /**
+     * @notice Appends component attributes to the existing JSON attributes string.
+     * @param attributes The current JSON string of attributes.
+     * @param componentAttributes An array of component attributes to be added.
+     * @param isFirst A boolean indicating if this is the first attribute being added.
+     * @return updatedAttributes The updated JSON string of attributes.
+     */
     function concatenateAttributes(string memory attributes, string[2] memory componentAttributes, bool isFirst)
         private
         pure
